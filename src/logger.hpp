2508e6bcf544f003b489dafe49bb8c070108cd4b
yusufemresamur <yusufemresamur01>
2026-05-05 18:49:14 +0000

    add the logger

---
+++ src/logger.hpp
@@ -0,0 +1,177 @@
+#pragma once
+#ifndef MLOGPP_LOGGER_HPP_
+#define MLOGPP_LOGGER_HPP_
+
+#include "fmt_string_with_location.hpp"
+#include "level.hpp"
+#include "record.hpp"
+#include "sink.hpp"
+#include <string>
+#include <vector>
+namespace mlogpp {
+
+/**
+ * @brief Logger class that manages log records and dispatches them to sinks.
+ * Each logger has a name and a minimum log level. Log records below the minimum
+ * level are ignored.
+ *
+ */
+class Logger {
+ public:
+  /**
+   * @brief Construct a Logger instance.
+   *
+   * @param name Name of the logger, used in log records to identify the source
+   * of logs.
+   * @param sinks List of sinks to which log records will be dispatched.
+   * @param min_level Minimum log level. Log records below this level will be
+   * ignored.
+   */
+  explicit Logger(std::string_view const name,
+                  std::vector<Sink> const& sinks = {},
+                  LogLevel const min_level = LogLevel::kInfo)
+      : name_(name), sinks_(sinks), min_level_(min_level) {};
+
+  /**
+   * @brief Add a sink to the logger.
+   *
+   * @param sink Sink to add.
+   * @return Logger& Reference to the logger, allowing for method chaining.
+   */
+  Logger& AddSink(Sink const&& sink) noexcept {
+    sinks_.push_back(sink);
+    return *this;
+  };
+
+  /**
+   * @brief Set the minimum log level for the logger. Log records below this
+   * level will be ignored.
+   *
+   * @param level Level to set as the minimum log level.
+   * @return Logger& Reference to the logger, allowing for method chaining.
+   */
+  Logger& SetMinLevel(LogLevel const level) noexcept {
+    min_level_ = level;
+    return *this;
+  };
+
+  /**
+   * @brief Return the name of the logger.
+   *
+   * @return std::string_view Name of the logger.
+   */
+  [[nodiscard]] std::string_view Name() const noexcept { return name_; };
+
+  /**
+   * @brief Log a message with the specified log level. If the log level is
+   * below the minimum log level of the logger, the message will be ignored.
+   *
+   * @tparam Level Log level of the message.
+   * @tparam Args Types of the arguments for the format string.
+   * @param msg Format string with location information.
+   * @param args Arguments for the format string.
+   */
+  template <LogLevel Level, typename... Args>
+  void Log(FormatStringWithLocation const msg, Args&&... args) {
+    if (Level < min_level_) {
+      return;
+    }
+    Emit(LogRecord{Level, msg.loc, Name(), msg.fmt,
+                   std::forward<Args>(args)...});
+  }
+
+  /**
+   * @brief Log a trace message.
+   *
+   * @tparam Args Types of the arguments for the format string.
+   * @param msg Format string with location information.
+   * @param args Arguments for the format string.
+   */
+  template <typename... Args>
+  void Trace(FormatStringWithLocation const msg, Args&&... args) {
+    Log<LogLevel::kTrace>(msg, std::forward<Args>(args)...);
+  }
+
+  /**
+   * @brief Log a debug message.
+   *
+   * @tparam Args Types of the arguments for the format string.
+   * @param msg Format string with location information.
+   * @param args Arguments for the format string.
+   */
+  template <typename... Args>
+  void Debug(FormatStringWithLocation const msg, Args&&... args) {
+    Log<LogLevel::kDebug>(msg, std::forward<Args>(args)...);
+  }
+
+  /**
+   * @brief Log an info message.
+   *
+   * @tparam Args Types of the arguments for the format string.
+   * @param msg Format string with location information.
+   * @param args Arguments for the format string.
+   */
+  template <typename... Args>
+  void Info(FormatStringWithLocation const msg, Args&&... args) {
+    Log<LogLevel::kInfo>(msg, std::forward<Args>(args)...);
+  }
+
+  /**
+   * @brief Log a warning message.
+   *
+   * @tparam Args Types of the arguments for the format string.
+   * @param msg Format string with location information.
+   * @param args Arguments for the format string.
+   */
+  template <typename... Args>
+  void Warn(FormatStringWithLocation const msg, Args&&... args) {
+    Log<LogLevel::kWarn>(msg, std::forward<Args>(args)...);
+  }
+
+  /**
+   * @brief Log an error message.
+   *
+   * @tparam Args Types of the arguments for the format string.
+   * @param msg Format string with location information.
+   * @param args Arguments for the format string.
+   */
+  template <typename... Args>
+  void Error(FormatStringWithLocation const msg, Args&&... args) {
+    Log<LogLevel::kError>(msg, std::forward<Args>(args)...);
+  }
+
+  /**
+   * @brief Log a fatal message.
+   *
+   * @tparam Args Types of the arguments for the format string.
+   * @param msg Format string with location information.
+   * @param args Arguments for the format string.
+   */
+  template <typename... Args>
+  void Fatal(FormatStringWithLocation const msg, Args&&... args) {
+    Log<LogLevel::kFatal>(msg, std::forward<Args>(args)...);
+  }
+
+ private:
+  /**
+   * @brief Emit a log record to all sinks. This is called by the Log method
+   * after checking the log level.
+   *
+   * @param r Log record to emit.
+   */
+  void Emit(LogRecord const& r) const noexcept {
+    for (Sink const& sink : sinks_) {
+      sink(r);
+    }
+  };
+
+  /// Name of the logger, used in log records to identify the source of logs.
+  std::string const name_;
+  /// List of sinks to which log records will be dispatched.
+  std::vector<Sink> sinks_;
+  /// Minimum log level. Log records below this level will be ignored.
+  LogLevel min_level_;
+};
+
+}  // namespace mlogpp
+#endif  // MLOGPP_LOGGER_HPP_
