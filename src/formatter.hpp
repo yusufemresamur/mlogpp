7aac5d77c136bf5ac3c598548177bc8ee5ce4f8d
yusufemresamur <yusufemresamur01>
2026-05-05 18:43:12 +0000

    add formatter concept and default formatter

---
+++ src/formatter.hpp
@@ -0,0 +1,42 @@
+#pragma once
+#ifndef MLOGPP_FORMATTER_HPP_
+#define MLOGPP_FORMATTER_HPP_
+
+#include "record.hpp"
+#include <concepts>
+namespace mlogpp {
+
+/**
+ * @brief Concept for formatter functions: any callable (const LogRecord&) ->
+ * std::string
+ *
+ * @tparam F Formatter function
+ */
+template <typename F>
+concept FormatterFunction = requires(F f, LogRecord const& r) {
+  { f(r) } -> std::convertible_to<std::string>;
+};
+
+/**
+ * @brief Default formatter for log records. Output format: "[timestamp] [level]
+ * [logger_name] [file:line] - message"
+ *
+ */
+struct DefaultFormatter {
+  [[nodiscard]] std::string operator()(LogRecord const& r) const {
+    return std::format("[{}] [{}] [{}] [{}:{}] - {}",
+                       std::chrono::duration_cast<std::chrono::milliseconds>(
+                           r.timestamp.time_since_epoch())
+                           .count(),
+                       ToString(r.level), r.logger_name, r.location.file_name(),
+                       r.location.line(), r.message);
+  };
+};
+static_assert(FormatterFunction<DefaultFormatter>,
+              "DefaultFormatter must satisfy FormatterFunction concept");
+
+// TODO (yusufemresamur) : Add more formatters, e.g., JSONFormatter,
+// XMLFormatter, etc.
+
+}  // namespace mlogpp
+#endif  // MLOGPP_FORMATTER_HPP_
