368f43007aa51f560e811907a32742e567bc551d
yusufemresamur <yusufemresamur01>
2026-05-06 17:32:55 +0000

    add simple file sink

---
+++ src/file_sink.hpp
@@ -0,0 +1,60 @@
+#pragma once
+#ifndef MLOGPP_FILE_SINK_HPP_
+#define MLOGPP_FILE_SINK_HPP_
+#include "src/formatter.hpp"
+#include "src/record.hpp"
+#include "src/sink.hpp"
+#include <filesystem>
+#include <fstream>
+
+namespace mlogpp {
+
+/**
+ * @brief A sink that writes log records to a file. The file is opened in append
+ * mode.
+ *
+ */
+class FileSink {
+ public:
+  /**
+   * @brief Construct a File Sink. Throws an exception if the file cannot be
+   * opened.
+   *
+   * @param path Path to the log file. The file will be created if it does not
+   * exist.
+   */
+  explicit FileSink(std::filesystem::path path) : path_(std::move(path)) {
+    file_.open(path_, std::ios::app);
+    if (!file_) {
+      throw std::runtime_error("Failed to open log file: " + path_.string());
+    }
+  };
+
+  /**
+   * @brief Write a log record to the file.
+   *
+   * @param record The log record to write.
+   */
+  void operator()(LogRecord const& record) {
+    file_ << DefaultFormatter{}(record) << std::endl;
+  };
+
+ private:
+  /// File stream for writing log records to the file.
+  std::fstream file_;
+  /// Path to the log file.
+  std::filesystem::path path_;
+};
+
+/**
+ * @brief Make a FileSink with the given path. See @c FileSink for details.
+ *
+ * @param path Path to the log file.
+ * @return Sink FileSink wrapped in a Sink type-erasure wrapper.
+ */
+[[nodiscard]] inline Sink MakeFileSink(std::filesystem::path path) {
+  return Sink{FileSink{std::move(path)}};
+}
+}  // namespace mlogpp
+
+#endif  // MLOGPP_FILE_SINK_HPP_
